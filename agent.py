51d009762c524526f2d980e33832df13441aaaae
credovian <cameron.redovian@r-dex.com>
2024-10-04 13:30:36 -0400

    implemented GAE and loss calculation

--- agent.py
+++ agent.py
@@ -3,9 +3,10 @@ import numpy as np
 
 
 class ActorCritic(torch.nn.Module):
-    def __init__(self, state_size, n_actions, gamma=0.99):
+    def __init__(self, state_size, n_actions, gamma=0.99, entropy_scale=0.01, tau=0.95):
         super(ActorCritic, self).__init__()
         self.gamma = gamma
+        self.entropy_scale = entropy_scale
 
         self.conv1 = torch.nn.Conv2d(
             state_size[0], 32, kernel_size=3, stride=2, padding=1
@@ -45,6 +46,59 @@ class ActorCritic(torch.nn.Module):
         log_prob = dist.log_prob(action)
 
         return action.numpy()[0], log_prob, v, hidden_state
+
+    def _calculate_returns(self, rewards, values, done):
+        values = torch.cat(values).squeeze()
+
+        if len(values.size()) == 1:  # batch of states
+            r = values[-1] * (1 - int(done))
+        elif len(values.size()) == 0:  # single state
+            r = values * (1 - int(done))
+
+        batch_return = []
+        for reward in rewards[::-1]:
+            r = reward + self.gamma * r
+            batch_return.append(r)
+
+        batch_return.reverse()
+        return torch.tensor(batch_return, dtype=torch.float).reshape(values.size())
+
+    def _generalized_advantage_estimation(self, delta_t):
+        n_steps = len(delta_t)
+        gae = np.zeros(n_steps)
+        for t in range(n_steps):
+            for k in range(0, n_steps - t):
+                gae[t] += (self.gamma * self.tau) ** k * delta_t[t + k]
+
+        return torch.tensor(gae, dtype=torch.float)
+
+    def calculate_cost(
+        self, next_state, hidden_state, rewards, values, log_probs, done
+    ):
+        returns = self._calculate_returns(rewards, values, done)
+
+        next_value = (
+            torch.zeros(1, 1)
+            if done
+            else self.forward(
+                torch.tensor([next_state], dtype=torch.float), hidden_state
+            )[1]
+        )
+        values.append(next_value.detach())  # detach to prevent backpropagation
+
+        values = torch.cat(values).squeeze()
+        log_probs = torch.cat(log_probs)
+        rewards = torch.tensor(rewards, dtype=torch.float)
+
+        delta_t = rewards + self.gamma * values[1:] - values[:-1]
+        gae = self._generalized_advantage_estimation(delta_t)
+
+        # squeeze to prevent rank error in case of single sample
+        critic_loss = torch.nn.functional.mse_loss(values[:-1].squeeze(), returns)
+        actor_loss = -(log_probs * gae).sum()
+        entropy_loss = (-log_probs * torch.exp(log_probs)).sum()
+
+        return actor_loss + critic_loss - self.entropy_scale * entropy_loss
 
 
 if __name__ == "__main__":
