7ffa199e7268b13b18afa4d3a0035066ead274cb
credovian <cameron.redovian@r-dex.com>
2024-10-06 16:03:39 -0400

    Update main script file

--- main.py
+++ main.py
@@ -1,2 +1,16 @@
 import os
-import gymnasium as gym
+import torch.multiprocessing as mp
+from parallel_env import ParallelEnv
+
+os.environ["OMP_NUM_THREADS"] = "1"
+
+if __name__ == "__main__":
+    mp.set_start_method("forkserver")
+    gloabl_ep = mp.Value("i", 0)
+
+    env_id = "PongNoFrameskip-v4"
+    n_threads = 8
+    n_actions = 6
+    input_shape = (4, 84, 84)
+
+    env = ParallelEnv(env_id, n_threads, input_shape, n_actions, gloabl_ep)
